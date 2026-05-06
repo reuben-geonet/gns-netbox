0356ad4252df5cd78592caa2f59f009961d71a73
reuben-geonet <r.andrews@gns.cri.nz>
2026-05-06 14:29:52 +1200

    Override default password validators

--- configuration/extra.py
+++ configuration/extra.py
@@ -54,3 +54,4 @@
 # from datetime import datetime
 # now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 # BANNER_TOP = f'<marquee width="200px">This instance started on {now}.</marquee>'
+AUTH_PASSWORD_VALIDATORS = []
